c3bf002d8dcf3d2cd34e2eb31b97b7de98996ec8
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2021-10-31 16:01:39 -0300

    Configurando paginação

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = { # control how many objects per page are returned
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
